7e4fba92e5e0aac37c9c8487c5c08030fd2d2ee5
Jonathandc33 <jonathan.brandaodc33@gmail.com>
2023-01-23 18:04:49 -0300

    atualização

---
+++ Sintaxe Java/java-anatomia-classes/src/Java/Aulas/anatomiaDasClasses/MinhaClasse.java
@@ -0,0 +1,21 @@
+package Java.Aulas.anatomiaDasClasses;
+
+public class MinhaClasse {
+
+public static void main (String [] args) {
+    String primeiroNome = "Jonathan";
+    String segundoNome = "Brandão";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println (nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do Método" + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
+    
+
+
---
+++ Sintaxe Java/java-anatomia-classes/src/Java/Aulas/tiposEVariaves/tipoeVariaveis.java
@@ -0,0 +1,15 @@
+package Java.Aulas.tiposEVariaves;
+
+public class tipoeVariaveis {
+    public static void main(String[] args) {
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;     
+        numero = 4 ;
+
+        System.out.print(numero);
+    }
+}
